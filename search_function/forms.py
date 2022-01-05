bb7996b3f3b71b8ed949aad94efd042bef642d71
Jack Cota <jack@cota.co.uk>
2022-01-05 02:21:35 +0000

    Fix search form
    This commit fixes the search form so that you can dynamically add fields to the form, and then submit it, and the data is printed to console to test it

--- search_function/forms.py
+++ search_function/forms.py
@@ -2,14 +2,11 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, IntegerField, FieldList, Form, FormField
 from wtforms.validators import InputRequired, ValidationError
 
-
 class PartForm(Form):
     part_name = StringField('Part Name', validators=[InputRequired()])
     quantity = IntegerField('Quantity', validators=[InputRequired()])
 
 
 class SearchForm(FlaskForm):
-    parts_dict = [{"part_name": "First part", "quantity": 0}]
-    parts = FieldList(FormField(PartForm), min_entries=1)
+    parts = FieldList(FormField(PartForm))
     submit = SubmitField()
-
--- search_function/views.py
+++ search_function/views.py
@@ -1,4 +1,5 @@
 import json
+import random
 from flask import Blueprint, render_template, session
 from search_function.forms import SearchForm
 from search_function.search import string_to_search_obj
@@ -8,22 +9,15 @@ from search_function.objects import Search, Part, Supplier
 def setup():
     search_test = Search()
     for i in range(100):
-        part_test = Part("4298673473274", 50)
+        part_test = Part(str(random.randint(3000000, 9999999)), 20)
         search_test.parts.append(part_test)
-        supplier_test = Supplier("Test Supplier")
-        supplier_test.stock = 85
-        supplier_test.price = 1.40
-        part_test.suppliers.append(supplier_test)
 
-        supplier_test2 = Supplier("Second Test Supplier")
-        supplier_test2.stock = 42
-        supplier_test2.price = 3.48
-        part_test.suppliers.append(supplier_test2)
+        for p in range(random.randint(2,10)):
 
-        supplier_test3 = Supplier("Third Test Supplier")
-        supplier_test3.stock = 4245
-        supplier_test3.price = 8.32
-        part_test.suppliers.append(supplier_test3)
+            supplier_test = Supplier("Test Supplier")
+            supplier_test.stock = random.randint(5,250)
+            supplier_test.price = random.randint(1,10)
+            part_test.suppliers.append(supplier_test)
 
     return search_test
 
@@ -39,6 +33,7 @@ def search():
         for field in form.parts:
             print(field.part_name.data)
             print(field.quantity.data)
+
 
     return render_template('search.html', form=form)
 
