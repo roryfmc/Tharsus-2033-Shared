b7166390c9a19fcf9512f646a275418199c9643b
Jack Cota <jack@cota.co.uk>
2022-01-11 13:07:45 +0000

    Add role to user in db

--- database/models.py
+++ database/models.py
@@ -7,10 +7,12 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(100), nullable=False)
+    role = db.Column(db.String(100), nullable=False)
 
-    def __init__(self, username, password):
+    def __init__(self, username, password, role):
         self.username = username
         self.password = password
+        self.role = role
 
 
 class WhitelistedEmail(db.Model):
