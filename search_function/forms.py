c5c52493050714fe6846f3988fd9154e9f9c1ec7
Jack Cota <jack@cota.co.uk>
2022-01-11 12:27:10 +0000

    Add docstring

--- search_function/forms.py
+++ search_function/forms.py
@@ -1,12 +1,27 @@
+"""This module stores the Flask forms used by the search function.
+The form SearchForm utilises the form PartForm within itself, using
+a FieldList to allow a dynamic number of part_names and quantities
+to be supplied in the form.
+"""
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, IntegerField, FieldList, Form, FormField
 from wtforms.validators import InputRequired, ValidationError
 
+
 class PartForm(Form):
+    """This form stores the data for each individual part entered
+    into the form. This form is used multiple times in the main form,
+    to accommodate for as many parts entered as the user desires.
+    """
     part_name = StringField('Part Name', validators=[InputRequired()])
     quantity = IntegerField('Quantity', validators=[InputRequired()])
 
 
 class SearchForm(FlaskForm):
+    """This is the main form used by the search function.
+    It utilises the PartForm a dynamic number of times to
+    create a form which allows the user to enter as many
+    parts, along with quantities, as they desire.
+    """
     parts = FieldList(FormField(PartForm))
     submit = SubmitField()
--- search_function/search.py
+++ search_function/search.py
@@ -1,8 +1,22 @@
+"""This module contains functions used by the search process.
+These functions include string_to_search_obj, which turns the json format
+stored in the session cookie as a string, into a Search object which can be used.
+This module also contains search_form_to_obj, which turns the flask form containing
+the user inputted data into a Search object.
+"""
+
 import json
 from search_function.objects import Search, Part, Supplier
 
 
 def string_to_search_obj(string_value):
+    """This function turns a string into a Search object.
+    This is used when the search data is retrieved from the session,
+    as it is in json format, stored as a string.
+
+    :param string_value: the json formatted search data stored in a string
+    :return: the Search object storing the search data.
+    """
     # Turn string json into a dict
     dictionary_value = json.loads(string_value)
 
@@ -31,11 +45,20 @@ def string_to_search_obj(string_value):
 
 
 def search_form_to_obj(search_form):
+    """This function takes the flask SearchForm
+    and returns a Search object with the relevant data stored.
+
+    :param search_form: Flask SearchForm
+    :return: Search object storing the data from the SearchForm
+    """
+    # Create new Search object
     search_result = Search()
 
+    # For each part inputted into the form
     for field in search_form.parts:
+        # Create new part object
         part = Part(field.part_name.data, field.quantity.data)
+        # Add the part object to the list in the Search object
         search_result.parts.append(part)
 
     return search_result
-
