757d2d01aaf8c856928933378e30cdd0a150e12e
Jack Cota <jack@cota.co.uk>
2022-01-11 16:52:10 +0000

    Add docstring

--- database/models.py
+++ database/models.py
@@ -1,8 +1,24 @@
-from app import db
+"""This module stores all of the models used by the database.
+These models are used when something is added to the database.
+"""
 from werkzeug.security import generate_password_hash
+from app import db
 
 
 class User(db.Model):
+    """This model represents a user in the database.
+    It should be called when you are handling users
+    from the database.
+
+    :cvar id: The incrementing ID number for each entity in the database
+    :type id: int
+    :cvar username: The username of the account
+    :type username: str
+    :cvar password: The password of the account
+    :type password: str
+    :cvar role: The role of the account, either user or admin
+    :type role: str
+    """
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -10,13 +26,22 @@ class User(db.Model):
     password = db.Column(db.String(100), nullable=False)
     role = db.Column(db.String(100), nullable=False)
 
-    def __init__(self, username, password, role):
+    def __init__(self, username, password, role='user'):
         self.username = username
         self.password = generate_password_hash(password)
         self.role = role
 
 
 class WhitelistedEmail(db.Model):
+    """This model represents a whitelisted email address in the database.
+    In the system, an admin has the ability to whitelist an email address,
+    which then allows a user to register with that email address.
+
+    :cvar id: The incrementing ID number for each entity in the database
+    :type id: int
+    :cvar email: The whitelisted email address
+    :type email: str
+    """
     __tablename__ = 'whitelisted_email'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -27,6 +52,20 @@ class WhitelistedEmail(db.Model):
 
 
 class PartPrice(db.Model):
+    """This model represents an entity which stores the price of
+    a part at a given time. This is used when a part has been searched,
+    and stores the lowest price of the part at that time, along with
+    the date and time.
+
+    :cvar id: The incrementing ID number for each entity in the database
+    :type id: int
+    :cvar part_id: The identifying name/ID of the part
+    :type part_id: str
+    :cvar price: The lowest price of the part at the given time
+    :type price: float
+    :cvar datetime: The datetime when the part is at this price. Defaults to current time
+    :type datetime: datetime
+    """
     __tablename__ = 'part_price'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -34,25 +73,50 @@ class PartPrice(db.Model):
     price = db.Column(db.Float, nullable=False)
     datetime = db.Column(db.DateTime, nullable=False)
 
-    def __init__(self, part_id, price, datetime):
+    def __init__(self, part_id, price, datetime=datetime.now()):
         self.part_id = part_id
         self.price = price
         self.datetime = datetime
 
 
 class DesiredPart(db.Model):
+    """This model represents a part which a user has added to their wishlist.
+    This means that they would like to be notified when the part has come into stock again.
+
+    :cvar id: The incrementing ID number for each entity in the database
+    :type id: int
+    :cvar user_id: The ID of the user who wishes to add the part to their wishlist
+    :type user_id: int
+    :cvar part_id: The identifying name/ID of the part
+    :type part_id: str
+    :cvar quantity: The desired quantity of the part
+    :type quantity: int
+    """
     __tablename__ = 'desired_parts'
 
     id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
     part_id = db.Column(db.String(100), nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
 
-    def __init__(self, part_id, quantity):
+    def __init__(self, user_id, part_id, quantity):
+        self.user_id = user_id
         self.part_id = part_id
         self.quantity = quantity
 
 
 class FavouriteSupplier(db.Model):
+    """This model represents a user's preferred supplier. When a supplier has
+    been preferred by the user, it then appears higher a search result when it is
+    included.
+
+    :cvar id: The incrementing ID number for each entity in the database
+    :type id: int
+    :cvar user_id: The ID of the user who wishes to add the supplier to their preferred suppliers
+    :type user_id: int
+    :cvar supplier_name: The name of the supplier which the user prefers
+    :type supplier_name: str
+    """
     __tablename__ = 'favourite_suppliers'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -65,6 +129,17 @@ class FavouriteSupplier(db.Model):
 
 
 class BlacklistedSupplier(db.Model):
+    """This model represents a user's blacklisted supplier. A supplier is blacklisted
+    by a user when they would not like to see results from this supplier. This means
+    the supplier won't show up in any of the user's search results.
+
+    :cvar id: The incrementing ID number for each entity in the database
+    :type id: int
+    :cvar user_id: The ID of the user who wishes to add the supplier to their blacklisted suppliers
+    :type user_id: int
+    :cvar supplier_name: The name of the supplier which the user wants to blacklist
+    :type supplier_name: str
+    """
     __tablename__ = 'blacklisted_suppliers'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -77,6 +152,23 @@ class BlacklistedSupplier(db.Model):
 
 
 class PartSearch(db.Model):
+    """This model represents an individual part which the user has searched for.
+    Since this model only represents an individual part, searches with multiple
+    parts must be broken down into multiple PartSearch objects, which should be connected
+    by containing the same datetime value.
+
+    :cvar id: The incrementing ID number for each entity in the database
+    :type id: int
+    :cvar user_id: The ID of the user who has searched for the part
+    :type user_id: int
+    :cvar part_id: The identifying name/ID of the part
+    :type part_id: str
+    :cvar quantity: The desired quantity of the part
+    :type quantity: int
+    :cvar datetime: The datetime of the search. Must match the datetime of other parts
+    in the same search
+    :type datetime: datetime
+    """
     __tablename__ = 'search_history'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -93,6 +185,11 @@ class PartSearch(db.Model):
 
 
 def init_db():
+    """This function is used to reset the database
+    and enter a test user entity.
+    To use this function, type "from database.models import init_db", then "init_db()"
+    in the python console.
+    """
     db.drop_all()
     db.create_all()
     new_user = User(username='user1@test.com', password='password', role='user')
